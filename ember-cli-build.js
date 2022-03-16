e3837aebf2696eaf7a23983b5bca2509e434232b
Mikayla Henrie <henriemik@gmail.com>
2022-03-16 14:55:42 -0600

    retry

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    minifyJS: {
+      options: {
+        exclude: ["**/sjcl/sjcl.js"]
+      }
+    }
     // Add options here
   });
 
